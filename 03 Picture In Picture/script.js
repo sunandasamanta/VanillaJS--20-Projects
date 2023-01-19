b91520d6090a7511f9b8f0f8bede2a27bea98d34
sunandasamanta <samantasunanda724@gmail.com>
2023-01-19 13:23:02 +0530

    Started to add functionality to the button

---
+++ 03 Picture In Picture/script.js
@@ -0,0 +1,17 @@
+// HTML elements
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select a media stream
+
+async function selectMediaStream () {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// On Load
+selectMediaStream();
